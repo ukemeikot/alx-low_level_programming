b93e8ded7d75860161fd3cd8b5e49b4e5ba9012b
Ukeme Ikot <ukemeetim2222@gmail.com>
2023-09-09 12:15:19 -0700

    yes

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main-the min functuion
+ * Return: always return 0
+ */
+
+int main(void)
+{
+	int num;
+	char l;
+
+	for (num = 0; num < 0; num++)
+		putchar((num % 10) + '0');
+	for (l = 'a'; l <= 'f'; l++)
+		putchar(l);
+	return (0);
+}
