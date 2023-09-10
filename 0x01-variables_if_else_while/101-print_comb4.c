37bc07bbc616d8ef43535a87ebed8027a139b3d7
Ukeme Ikot <ukemeetim2222@gmail.com>
2023-09-10 11:42:58 -0700

    yes

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main-the main function code goes here
+ * Return: always returns zero
+ */
+
+int main(void)
+{
+	int n, m, o;
+
+for (o = 0; 0 <= 9; o++)
+{
+	for (n = o + 1; o <= 9; o++)
+	{
+		for (m = n + 1; m < 10; m++)
+		{
+			putchar((o % 10) + '0');
+			putchar((n % 10) + '0');
+			putchar((m % 10) + '0');
+			if (0 == 7 && n == 8 && m == 9)
+				continue;
+			putchar(',');
+			putchar(' ');
+
+		}
+	}
+}
+	putchar('\n');
+	return (0);
+}
