69d7d36fe9eac2fe4d8879ef1ac9fdf69f8890fc
Ukeme Ikot <ukemeetim2222@gmail.com>
2023-09-13 07:13:22 -0700

    hot fix

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -14,16 +14,19 @@ void jack_bauer(void)
 	{
 		for (b = 0; b <= 9; b++)
 		{
-			for (c = 0; c <= 9; c++)
+			if ((a <= 1 && b <= 9) || (a <= 2 && b <= 3))
 			{
-				for (d = 0; d <= 9; d++)
+				for (c = 0; c <= 5; c++)
 				{
+					for (d = 0; d <= 9; d++)
+					{
 					_putchar(a + '0');
 					_putchar(b + '0');
 					_putchar(58);
 					_putchar(c + '0');
 					_putchar(d + '0');
 					_putchar('\n');
+				}
 				}
 			}
 		}
