042d80bfb688815bc21d555da765877815364d39
Ukeme Ikot <ukemeetim2222@gmail.com>
2023-09-13 15:40:19 -0700

    sum of even fibo

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 
 /**
- * main - prints the required sum
- * return: 0
+ * main-print sum of even fibonacii numbers
+ * less than 4000000
+ * Return: 0.
  */
 
 int main(void)
@@ -10,7 +11,7 @@ int main(void)
 	int n = 0;
 	long a = 1, b = 2, sum = b;
 
-	while (a + b < 400000)
+	while (a + b < 4000000)
 	{
 		b += a;
 		if (b % 2 == 0)
