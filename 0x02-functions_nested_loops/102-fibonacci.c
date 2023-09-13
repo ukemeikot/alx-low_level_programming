60bb65b4c2a7cc47ddd395748bc897e42339d1dc
Ukeme Ikot <ukemeetim2222@gmail.com>
2023-09-13 15:04:27 -0700

    fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - prints fibonacci numbers
+ * Return: o on success
+ */
+
+int main(void)
+{
+	int n = 0;
+	long a = 1, b = 2;
+
+	while (n < 50)
+	{
+		if (n == 0)
+			printf("%ld", a);
+		else if (n == 1)
+		{
+			printf(", %ld", b);
+		}
+		else
+		{
+			a += b;
+			b = a - b;
+			printf(", %ld", b);
+		}
+		n++;
+	}
+	printf("\n");
+	return (0);
+}
