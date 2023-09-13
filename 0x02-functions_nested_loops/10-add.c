1d4fe09188301ba53c484b14889557b956fdd761
Ukeme Ikot <ukemeetim2222@gmail.com>
2023-09-13 08:09:03 -0700

    carries out addition

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - adds two integers
+ * @a: first interger
+ * @b: secong integer
+ * Return: returns the sum
+ */
+
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+	return (sum);
+}
