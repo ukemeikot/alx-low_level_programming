adcc853da139ae19f413e2096955e7abf76efed4
Ukeme Ikot <ukemeetim2222@gmail.com>
2023-09-15 00:35:35 -0700

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - print numbers from 1 - 9
+ * Return: nothing
+ */
+
+void print_numbers(void)
+{
+	int c;
+
+	for (c = 0; c < 10; c++)
+		_putchar(c);
+	_putchar('\n');
+}
+
