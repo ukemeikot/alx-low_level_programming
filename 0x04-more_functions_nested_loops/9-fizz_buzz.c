0f18426c8c427c1abb99fada60859660cf893a18
Ukeme Ikot <ukemeetim2222@gmail.com>
2023-09-15 08:28:05 -0700

    fix

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,16 +12,16 @@ int main(void)
 
 	for (a = 1; a <= 100; a++)
 	{
-		if ((a % 3 == 0) && (a % 5 == 0) && a != 100)
-			printf("FizzBuzz ");
+		if ((a % 3 == 0) && (a % 5 == 0))
+			printf(" FizzBuzz");
 		else if ((a % 3 == 0) && (a % 5 != 0))
-			printf("Fizz ");
+			printf(" Fizz");
 		else if ((a % 5 == 0) && (a % 3 != 0))
-			printf("Buzz ");
-		else if (a == 100)
-			printf("Buzz");
+			printf(" Buzz");
+		else if (a == 1)
+			printf("%d", a);
 		else
-			printf("%d ", a);
+			printf(" %d", a);
 	}
 	printf("\n");
 	return (0);
