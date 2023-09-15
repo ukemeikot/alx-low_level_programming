d94e622eb4bde45b8153ddc569ca575cbe9de451
Ukeme Ikot <ukemeetim2222@gmail.com>
2023-09-15 00:04:41 -0700

    checks capital letters

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *_isupper - prints the upper character
+ * @c: the character to be checked.
+ * Return: returns 1 for uppercase and 0 otherwise
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{	
+		return(1);
+	}
+	else
+	{
+		return(0);
+	}
+
+}
