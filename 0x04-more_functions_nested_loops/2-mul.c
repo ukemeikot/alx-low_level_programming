69f1dbe55a2fc0ad2d60bb5c57d1a486571d43f5
Ukeme Ikot <ukemeetim2222@gmail.com>
2023-09-15 00:30:10 -0700

    multiplier

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ * @a: first number
+ * @b: second number
+ * Return: product
+ */
+
+int mul(int a, int b)
+{
+	int prod;
+
+	prod  = a * b;
+	return (prod);
+}
