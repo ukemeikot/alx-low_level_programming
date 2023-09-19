7d701e2b58beda8595409bee88b62bbae63e69b7
Ukeme Ikot <ukemeetim2222@gmail.com>
2023-09-19 10:30:53 -0700

    fixed

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -17,13 +17,11 @@ void puts_half(char *str)
 		length_of_the_string++;
 	}
 
-	
 		n = (length_of_the_string - 1) / 2;
 
 	for (i = n + 1; i < length_of_the_string; i++)
-		{
-
-			_putchar(str[i]);
-		}
+	{
+		_putchar(str[i]);
+	}
 	_putchar('\n');
 }
