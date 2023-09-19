affb63cdbc080dbb734f14b304528d1e4e6bf716
Ukeme Ikot <ukemeetim2222@gmail.com>
2023-09-19 09:59:45 -0700

    half string print

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * puts_half - to print half of the string
+ * @str: the character holder
+ * Return: nothing
+ */
+
+void puts_half(char *str)
+{
+	int length_of_the_string = 0;
+	int n;
+	int i;
+
+	while (str[length_of_the_string] != '\0')
+	{
+		length_of_the_string++;
+	}
+
+	if (length_of_the_string  % 2 == 0)
+	{
+		n = length_of_the_string / 2;
+	}
+	else
+	{
+		n = (length_of_the_string - 1) / 2;
+	}
+	for (i = n; i < length_of_the_string; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
