be33ea92b001e489dd02d6c961a8f5fe5c396461
Ukeme Ikot <ukemeetim2222@gmail.com>
2023-09-19 00:15:53 -0700

    resetting value stored in a pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - resent the number stored in a pointter to 98
+ * @n: bthe variable holding the number
+ * Return: returns 0
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This folder contains project on pointers, strings and arrays
