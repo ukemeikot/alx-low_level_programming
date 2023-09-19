99c73aec17462d08d2fe12147f7c666b8b075924
Ukeme Ikot <ukemeetim2222@gmail.com>
2023-09-19 09:01:02 -0700

    print every other string

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * puts2 - function to print every other character
+ * @str: the string holder
+ * Return: nothing
+ */
+
+void puts2(char *str)
+{
+	int length = 0;
+	while (str[length] != '\0')
+	{
+		_putchar(str[length]);
+		length = length + 2;
+	}
+	_putchar('\n');
+}
