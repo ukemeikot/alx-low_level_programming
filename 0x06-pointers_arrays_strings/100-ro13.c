66b0156b9c63088c44138a0513ebf822fd89152a
Ukeme Ikot <ukemeetim2222@gmail.com>
2023-09-21 07:20:11 -0700

    encoding software

---
+++ 0x06-pointers_arrays_strings/100-ro13.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * rot13 - main function
+ *
+ * @s: String poiner
+ *
+ * Return: String function
+ */
+
+char *rot13(char *s)
+{
+	int k, r;
+	char data1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char datarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (k = 0; s[k] != '\0'; k++)
+	{
+	for (r = 0; r < 52; r++)
+	{
+	if (s[k] == data1[r])
+	{
+	s[k] = datarot[r];
+	break;
+	}
+	}
+	}
+	return (s);
+}
+
