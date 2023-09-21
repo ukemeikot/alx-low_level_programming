d0f201bf648ef83a060c6933f8792254852f49d3
Ukeme Ikot <ukemeetim2222@gmail.com>
2023-09-21 03:18:00 -0700

    fixed

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -4,6 +4,7 @@
 *_strncat - concatenate with given bytes limit
 *@dest: destination string
 *@src: source string
+*@n: the number of elements to be copied.
 *Return: dest string
 */
 
@@ -11,12 +12,12 @@ char *_strncat(char *dest, char *src, int n)
 {
 	int i = 0;
 	int j = 0;
-	
+
 	while (dest[i] != '\0')
 	{
 		i++;
 	}
-	while (j < n && src[j] != '\0')
+	while ((j < n) && (src[j] != '\0'))
 	{
 		dest[i] = src[j];
 		i++;
