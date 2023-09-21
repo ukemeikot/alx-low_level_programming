219cd883f9a70c7b60af77e698ec098961ac57d7
Ukeme Ikot <ukemeetim2222@gmail.com>
2023-09-21 04:06:41 -0700

    fixed

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,13 +7,13 @@
 *Return: the difference between the two
 */
 
-int _strcmp (char *s1, char *s2)
+int _strcmp(char *s1, char *s2)
 {
 	int i;
 
-	for (i = 0; (s1[i] != '\0' || s2[i] != '\0'); i++) 
+	for (i = 0; (s1[i] != '\0' || s2[i] != '\0'); i++)
 	{
-		if (s1[i] != s2[i]) 
+		if (s1[i] != s2[i])
 		{
 			return ((int)(unsigned char)s1[i] - (int)(unsigned char)s2[i]);
 		}
