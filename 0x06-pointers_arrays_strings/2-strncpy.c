e1c094564bc3a200bf0acb4c6ac4d6cc9164d4fc
Ukeme Ikot <ukemeetim2222@gmail.com>
2023-09-21 01:38:43 -0700

    fixes

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -4,6 +4,7 @@
  * _strncpy - function that cpopies strings in c
  * @dest: destination string
  * @src: source string
+ * @n: the number of charcters in the string to be copied
  * Return: the destination string
  */
 
@@ -20,6 +21,7 @@ char *_strncpy(char *dest, char *src, int n)
 	{
 		dest[k] = src[k];
 	}
+	dest[k] = '\0';
 	return (dest);
 }
 
