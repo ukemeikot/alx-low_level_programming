f244bf2aba48e867de6e0fa42c930622dfa71f87
Ukeme Ikot <ukemeetim2222@gmail.com>
2023-09-26 03:03:52 -0700

    more on strings

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strspn - function to get the length of a suffix
+ * @s: the entered string
+ * @accept: the host for the suffix
+ * Return: number of bytes
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i = 0;
+	int k;
+
+	while (*s)
+	{
+	for (k = 0; accept[k]; k++)
+	{
+	if (*s == accept[k])
+	{
+	i++;
+	break;
+	}
+	else if (accept[k + 1] == '\0')
+	return (i);
+	}
+	s++;
+	}
+	return (i);
+	
+}
