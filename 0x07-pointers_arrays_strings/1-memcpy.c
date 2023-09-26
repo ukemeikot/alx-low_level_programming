a7bbc1a135dda9323ac7fb2e1dfe878b3b1314ca
Ukeme Ikot <ukemeetim2222@gmail.com>
2023-09-26 02:41:09 -0700

    fixed issues with task 1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -2,7 +2,7 @@
 
 /**
  * _memcpy - to copy n bytes from a memory area
- * @dest: destination 
+ * @dest: destination
  * @src: source
  * @n: number of bytes
  * Return: the destination address
@@ -12,7 +12,7 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int a;
 
-	for (a = 0; (a < n && src[a] != '\0'); a++)
+	for (a = 0; a < n; a++)
 		dest[a] = src[a];
 	return (dest);
 }
