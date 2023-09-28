d3ae85d25e89982395cc6d35729905087492e3bd
Ukeme Ikot <ukemeetim2222@gmail.com>
2023-09-28 03:13:31 -0700

    factorial printing

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * factorial -finds the factorial of a number
+ * @n: the number
+ * Return: the value of the factorial
+ */
+
+int factorial(int n)
+{
+	if (n == 0)
+		return (1);
+	if (n < 0)
+		return (-1);
+	return (n * factorial (n-1));
+}
