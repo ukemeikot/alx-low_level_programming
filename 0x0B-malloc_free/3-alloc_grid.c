1e08566c5a39c5d9c600a941938c0b0ee9976d6b
Ukeme Ikot <ukemeetim2222@gmail.com>
2023-10-03 07:10:07 -0700

    fix

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -18,7 +18,7 @@ int **alloc_grid(int width, int height)
 
 	if (width == 0 || height == 0)
 		return (NULL);
-	array = malloc(sizeof(int) * height);
+	array = malloc(sizeof(int*) * height);
 	if (array == NULL)
 		return (NULL);
 	for (a = 0; a < height; a++)
