ff00411bf0023f49d1e29148cb9de549a7d1412e
Ukeme Ikot <ukemeetim2222@gmail.com>
2023-10-03 07:21:51 -0700

    frees the array created with all_array

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * free_grid - frees 2d array
+ * @grid:the grid to be cleared.
+ * @height: height dimension of grid
+ * Description: frees memory of grid
+ * Return: nothing
+ *
+ */
+
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+	free(grid);
+}
+
