cca742bb5aa1ee384062e4760c9fe70d2f183237
ukemeikot <ukemeetim2222@gmail.com>
2023-10-10 18:59:29 +0100

    structures

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+this folder contains code on typedef and structures
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DO_H
+#define DOG_H
+/**
+ * struct dog - structure to hold the attributes of dogs
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the name of the dog owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
