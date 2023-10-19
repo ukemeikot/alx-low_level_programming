8a4f9b570d68583157e6502b2e49f218e425a22d
ukemeikot <ukemeetim2222@gmail.com>
2023-10-19 21:48:30 +0100

    printing first

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+ * prefunction - executes before a main function.
+ * Return: nothing
+ */
+void prefunction(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
