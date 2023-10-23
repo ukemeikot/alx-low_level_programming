8fdfabeab8f7d8286ad6e80a882bb4844e9c302c
ukemeikot <ukemeetim2222@gmail.com>
2023-10-23 18:45:06 +0100

    fixes

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,16 +8,15 @@
  */
 void free_listint2(listint_t **head)
 {
-	listint_t *temp, *current;
+	listint_t *temp;
 
 	if (*head == NULL)
 		return;
-	temp = *head;
-	while (temp != NULL)
+	while (*head != NULL)
 	{
-		current = temp->next;
-		free(temp);
-		temp = current;
+		temp = (*head)->next;
+		free(*head);
+		*head = temp;
 	}
 
 	*head = NULL;
