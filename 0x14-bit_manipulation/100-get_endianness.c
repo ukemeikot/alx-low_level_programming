8482919c62edca858d8efb1e3a66ac07f4a25305
ukemeikot <ukemeetim2222@gmail.com>
2023-10-26 14:12:23 +0100

    endianness

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * get_endianness - gets the type of endianness
+ * Return: 0 for small and 1 for big
+ */
+
+int get_endianness(void)
+{
+	unsigned t_value = 1;
+	char *byte_ptr = (char *) & t_value;
+
+	return (byte_ptr[0] == 1) ? 1 : 0;
+}
