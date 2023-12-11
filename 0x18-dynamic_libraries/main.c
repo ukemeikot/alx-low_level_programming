5f0239bd221cde8efe7d5a65fe629abfbba92b9c
ukemeikot <ukemeetim2222@gmail.com>
2023-12-11 16:27:07 +0100

    python

---
+++ 0x18-dynamic_libraries/main.c
@@ -0,0 +1,61 @@
+#include "main.h"
+
+/**
+ * add - add two numbers
+ * @a: num1
+ * @b: num2
+ * Return: sum of a and b
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * mul - muliply two numbers
+ * @a: num1
+ * @b: num2
+ * Return: a x b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - divide two numbers
+ * @a: num1
+ * @b: num2
+ * Return: a / b
+ */
+
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * sub - subtract two numbers
+ * @a: num1
+ * @b: num2
+ * Return: a - b
+ */
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mod - find remainder of two numbers
+ * @a: num1
+ * @b: num2
+ * Return: a % b
+ */
+
+int mod(int a, int b)
+{
+	return (a % b);
+}
